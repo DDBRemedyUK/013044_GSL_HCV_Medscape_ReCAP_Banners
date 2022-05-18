33afb4dd64668c87745a7ab675ee455a6656cfd0
Bernardo Baldiviezo <fataldesign616@gmail.com>
2022-05-18 12:25:18 +0100

    mpu banner coded

--- mpu/js/script.js
+++ mpu/js/script.js
@@ -5,38 +5,38 @@ $(document).ready(function () {
         idleTime = 0,
             idleInterval = setInterval(timerIncrement, 200),
             slide = 1,
-            slideTotal = 5,
+            slideTotal = 6,
             seconds = 0;
 
         var slides = {
-            frame1: 5,
-            frame2: 5, //multiples of 0.2
-            frame3: 5,
-            frame4: 5,
-            frame5: 0
+            frame1: 3,
+            frame2: 3, //multiples of 0.2
+            frame3: 3,
+            frame4: 4,
+            frame5: 3,
+            frame6: 0
         }
 
         //Increment the idle time counter every 3 seconds.
         function timerIncrement() {
             idleTime++;
             seconds = parseFloat(((idleTime * 0.2) % slides["frame" + slide]).toFixed(1)); //fix floating point number glitch
-            console.log(seconds);
             if (seconds == 0) {
                 let frame = "#frame" + (slide + 1);
 
-                if (frame === "#frame3") {
-                    $("#footer").addClass('show');
+                console.log(frame);
+
+                if(frame === "#frame5"){
+                    $("#frame5").prevAll().addClass("hide");
+                    $("#frame" + (slide + 1)).addClass("show");
+                    $("#footer1").addClass("hide");
+                    $("#footer2").addClass("show");
                 } 
-                if (frame === "#frame4") {
-                    $("#slides").removeClass('question');
-                } 
-                if (frame === "#frame5") {
-                    $("#logo-epclusa").addClass('hide');
+                if(frame === "#frame6"){
+                    $("#frame5").addClass("hide");
+                    $("#frame6").addClass("show");
                 }
-                $("#frame" + (slide + 1)).prev().addClass("hide");
                 $("#frame" + (slide + 1)).addClass("show");
-                $("#img" + (slide + 1)).prev().addClass("hide");
-                $("#img" + (slide + 1)).addClass("show");
 
                 seconds = idleTime = 0;
                 slide != slideTotal - 1 ? slide++ : clearInterval(idleInterval);
