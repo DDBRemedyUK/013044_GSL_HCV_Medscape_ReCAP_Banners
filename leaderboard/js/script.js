bbcf67eb1b592380887392d8044d2b0eee0f48d0
Bernardo Baldiviezo <fataldesign616@gmail.com>
2022-05-18 15:31:56 +0100

    starts work on leaderboard

---
+++ leaderboard/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    animateBanner()
+
+    function animateBanner() {
+        idleTime = 0,
+            idleInterval = setInterval(timerIncrement, 200),
+            slide = 1,
+            slideTotal = 4,
+            seconds = 0;
+
+        var slides = {
+            frame1: 2,
+            frame2: 3, //multiples of 0.2
+            frame3: 3,
+            frame4: 0
+        }
+
+        //Increment the idle time counter every 3 seconds.
+        function timerIncrement() {
+            idleTime++;
+            seconds = parseFloat(((idleTime * 0.2) % slides["frame" + slide]).toFixed(1)); //fix floating point number glitch
+            if (seconds == 0) {
+                let frame = "#frame" + (slide + 1);
+
+                console.log(frame);
+
+                if(frame === "#frame5"){
+                    $("#frame5").prevAll().addClass("hide");
+                    $("#frame" + (slide + 1)).addClass("show");
+                    $("#footer1").addClass("hide");
+                    $("#footer2").addClass("show");
+                } 
+                if(frame === "#frame6"){
+                    $("#frame5").addClass("hide");
+                    $("#frame6").addClass("show");
+                }
+                $("#frame" + (slide + 1)).addClass("show");
+
+                seconds = idleTime = 0;
+                slide != slideTotal - 1 ? slide++ : clearInterval(idleInterval);
+            }
+        };
+    }; //animateBanner()
+}); //document ready
